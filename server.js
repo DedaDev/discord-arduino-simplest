caac4786f5a9182c9fb7102483257545420c7c54
Deda <boyan.dedic@gmail.com>
2019-11-08 12:20:24 +0100

    Update server.js
    👍 🍔 🍡

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const five = require('johnny-five');
 const client = new Discord.Client();
-board = new five.Board();
-board.on("ready", ()=>{
-let led = new five.Led(13);
+const board = new five.Board();
+board.on("ready", () => {
+	const led = new five.Led(13);
 	client.on('message', msg => { 
 		if (msg.content === "led on") led.on()
 		else if (msg.content === "led off") led.off()
